ba3d33b3b01cd68c768cff99a43e72f4e1705a19
zhsy <zhsy1985@sina.com>
2017-10-24 16:38:05 +0800

    all service

--- ecabinet/src/main/java/com/yd/ecabinet/Entry.java
+++ ecabinet/src/main/java/com/yd/ecabinet/Entry.java
@@ -1,6 +1,9 @@
 package com.yd.ecabinet;
 
+import com.yd.ecabinet.monitor.MonitorService;
 import com.yd.ecabinet.redis.RedisListener;
+import com.yd.ecabinet.rfid.RfidService;
+import com.yd.ecabinet.rfid.order.TagService;
 import com.yd.ecabinet.util.BeanFactory;
 import com.yd.ecabinet.util.LoggerUtils;
 import org.slf4j.Logger;
@@ -12,20 +15,20 @@ public class Entry {
     private static final Logger logger = LoggerUtils.getLogger(Entry.class);
 
     public static void main(String[] args) throws InterruptedException {
-//        //connect rfid
-//        BeanFactory.getBean(RfidService.class).startup();
-//
-//        //init tags
-//        BeanFactory.getBean(TagService.class).init();
+        //connect rfid
+        BeanFactory.getBean(RfidService.class).startup();
+
+        //init tags
+        BeanFactory.getBean(TagService.class).init();
 
         //subscribe redis message for open door by rfid
         BeanFactory.getBean(RedisListener.class).startup();
-//
-//        MonitorService monitorService = BeanFactory.getBean(MonitorService.class);
-//        //monitor rfid status
-//        monitorService.watch();
-//        //report host status
-//        monitorService.report();
+
+        MonitorService monitorService = BeanFactory.getBean(MonitorService.class);
+        //monitor rfid status
+        monitorService.watch();
+        //report host status
+        monitorService.report();
 
         BeanFactory.destroy();
 
